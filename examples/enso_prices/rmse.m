7df00245b281d8536875a035e7c3290586d54395
Dimitrios Giannakis <dg227@dimitris.cims.nyu.edu>
2020-02-06 11:09:35 -0500

    Removed broken examples

--- examples/enso_prices/rmse.m
+++
@@ -1,6 +0,0 @@
-function err = rmse( x, y )
-%RMSE Compute RMSE between time series. Time is taken to be the second 
-% second dimension of the arrays.
-
-nS  = size( x, 2 );
-err = vecnorm( x - y, 2, 2 ) / sqrt( nS );
